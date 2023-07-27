acddd23ec145e2d3a5c2201c2793a681a4e0011e
marat-b <buran17@mail.ru>
2023-07-27 16:14:44 +0500

    payload

--- tgbot/utilz/payload_parser.py
+++ tgbot/utilz/payload_parser.py
@@ -2,7 +2,8 @@ def payload_parser(payload: str):
     words = payload.split('_')
     if len(words) == 3:
         customer_id = words[0]
-        if words[1] == 'participant':
+        if words[1] == 'p': # articipant
+            print(f'participant_number={words[2]}')
             return {"client_id":words[0], "participant_number":words[2]}
         elif words[1] == 'customer':
             return {"client_id":words[0], "customer_number":words[2]}
